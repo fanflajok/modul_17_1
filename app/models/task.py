f79808762d685fe33bd94524d2ff2322b33c26ce
Victor <fanflajok@gmail.com>
2025-01-05 22:59:43 +0300

    Update task.py

--- app/models/task.py
+++ app/models/task.py
@@ -5,9 +5,9 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     content = Column(String)
-    priority = Column(String, default=0)
+    priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('tasks.id'), nullable=False, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
     slug = Column(String, unique = True, index = True)
     user = relationship('User', back_populates='tasks')
 
